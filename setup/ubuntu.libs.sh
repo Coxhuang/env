3b0d94d9e9aff1540ee198b7b93787df510a273f
minhanghuang <job@minhang.me>
2024-07-05 08:50:17 +0800

    perf(setup): update Ubuntu dependency libs

--- setup/ubuntu.libs.sh
+++ setup/ubuntu.libs.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-
+#
 set -e
+
+# 执行当前脚本
+# wget -qO- https://raw.githubusercontent.com/minhanghuang/env/dev/setup/ubuntu.libs.sh | bash
+
+UBUNTU_VERSION=$(lsb_release -rs)
+IFS='.' read -r MAJOR_VERSION MINOR_VERSION <<< "$UBUNTU_VERSION"
+MAJOR_VERSION=${MAJOR_VERSION%.*}
+echo "当前Ubuntu版本: $MAJOR_VERSION"
 
 sudo apt update
 
@@ -11,22 +19,31 @@ sudo apt install -y \
 
 # install C++ dependence
 sudo apt install -y \
-  pkg-config autoconf automake cmake make g++ gdb cgdb clang clang-format ninja-build \
-  llvm bear valgrind
+  pkg-config autoconf automake cmake make ninja-build g++ gdb cgdb clang clang-format \
+  llvm bear valgrind apport
 
 # install tools
-sudo apt install -y \
-  htop chrony tig tree libunwind-dev curl unzip wget baobab xclip
+sudo apt install -y htop chrony tig tree libunwind-dev curl unzip wget baobab xclip
 
 # install pulgin
-sudo apt install -y \
-  zsh git vim nodejs npm yarn ripgrep universal-ctags global neofetch cppman
+sudo apt install -y zsh git vim nodejs yarn global neofetch cppman
+if [ "$MAJOR_VERSION" -gt 18 ]; then
+  # Ubuntu18以上
+  sudo apt install -y ripgrep universal-ctags
+fi
 
 # install porxy
 sudo apt install -y supervisor nginx
 
 # install language
-sudo apt install -y language-pack-zh-hans
+sudo apt install -y locales language-pack-zh-hans
+sudo locale-gen en_US.UTF-8 zh_CN.UTF-8
+
+if dpkg -l | grep -q "ubuntu-desktop" || dpkg -l | grep -q "gnome-shell"; then
+  echo "Ubuntu Desktop"
+else
+  echo "Ubuntu Server"
+fi
 
 # nfs-common: nfs目录挂载
 # baobab: 查看磁盘占用GUI
@@ -34,5 +51,6 @@ sudo apt install -y language-pack-zh-hans
 # universal-ctags global: 源代码导航和标签生成的工具
 # neofetch: 查看系统信息
 # cppman: 查看C++ 98/11/14/17/20 manual pages
+# apport: 解压crash文件
 # valgrind: 查看C++程序内存泄露工具
 # xclip: 剪切板工具
